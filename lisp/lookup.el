3b4788646ffa5e59fd496f10189ed241305d36b4
kawabata <kawabata>
2009-03-31 15:37:50 +0000

    apply lookup-auto-lookup codes. (test). fix byte-compiling problem.

--- lisp/lookup.el
+++ lisp/lookup.el
@@ -23,12 +23,13 @@
 
 ;;; Code:
 
-(load "lookup-autoloads")
-
 (require 'cl)
 (require 'lookup-types)
 (require 'lookup-utils)
 (require 'lookup-vars)
+
+(unless lookup-byte-compiling
+  (load "lookup-autoloads"))
 
 (defconst lookup-version "1.99.3"
   "The version numbers of Lookup.")
@@ -588,7 +589,7 @@ candidates."
 ;; reference
 
 (defun lookup-arrange-references (entry)
-  "Arrange reference of ENTRY.  
+  "Arrange reference of ENTRY.
 :reference-pattern should be (regexp link-item heading code).
 link-item, heading, or code may be integer or function."
   (let* ((dict (lookup-entry-dictionary entry))
@@ -895,6 +896,57 @@ If there is no session, default module will be returned."
 ;;;###autoload
 (defun lookup-use-support (id file)
   (lookup-assoc-set 'lookup-support-alist id file))
+
+;;;
+;;; Lookup Auto
+;;;
+
+(defvar lookup-auto-lookup-mode nil)
+(defvar lookup-auto-lookup-timer nil)
+(defvar lookup-auto-lookup-word "")
+
+(defun lookup-toggle-auto-lookup()
+  (interactive)
+  (if lookup-auto-lookup-mode 
+      (lookup-deactivate-auto-lookup)
+    (lookup-activate-auto-lookup)))
+
+(defun lookup-activate-auto-lookup ()
+  "Activate Auto Lookup."
+  (interactive)
+  (setq lookup-auto-lookup-mode nil)
+  (unless (lookup-get-module "auto")
+    (error "Please prepare `auto' module before using auto-lookup!"))
+  (setq lookup-auto-lookup-mode t)
+  (setq lookup-auto-lookup-timer
+        (run-with-idle-timer
+         1 t
+         'lookup-auto-lookup)))
+
+(defun lookup-deactivate-auto-lookup ()
+  "Deactivate Auto Lookup."
+  (interactive)
+  (setq lookup-auto-lookup-mode nil)
+  (cancel-timer lookup-auto-lookup-timer))
+
+(defun lookup-auto-lookup ()
+  "Execute Auto Lookup."
+  (when (and (not isearch-mode)
+             (not executing-kbd-macro)
+             lookup-auto-lookup-mode
+             (not (window-minibuffer-p (selected-window)))
+             (not (eq (current-buffer) (get-buffer (lookup-summary-buffer))))
+             (not (eq (current-buffer) (get-buffer (lookup-content-buffer))))
+             (not (eq (current-buffer) (get-buffer " *Dictionary List*")))
+             (not (eq (current-buffer) (get-buffer " *Search History*")))
+             (not (eq (current-buffer) (get-buffer " *Module List*"))))
+    (save-selected-window
+      (let ((lookup-edit-input nil)
+            (word (lookup-current-word))
+            (lookup-open-function 'lookup-other-window))
+        (when (not (equal lookup-auto-lookup-word word))
+          (lookup-word word (lookup-get-module "auto"))
+          (setq lookup-auto-lookup-word word))))))
 
 
 ;;;
