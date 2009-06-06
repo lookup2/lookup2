58639313acba1fe6392d7a82f39c9d2089ffd198
kawabata <kawabata>
2009-06-06 18:49:14 +0000

    modified to support error message entry.

--- lisp/nducs.el
+++ lisp/nducs.el
@@ -46,7 +46,7 @@
 
 (put 'nducs :charsets '(ascii))
 
-(put 'nducs :methods '(exact prefix suffix substring regexp))
+(put 'nducs :methods '(exact prefix suffix substring wildcard regexp))
 
 (put 'nducs :default-method 'substring)
 
@@ -64,6 +64,7 @@
   (let ((regexp (lookup-query-to-regexp query))
         (case-fold-search t)
         entries)
+    (message "debug: regexp=%s" regexp)
     (dolist (ucs-name (ucs-names))
       (if (string-match regexp (car ucs-name))
           (setq entries (cons (lookup-new-entry
@@ -71,26 +72,27 @@
                                (char-to-string (cdr ucs-name)) (car ucs-name))
                               entries))))
     (if (> (length entries) lookup-max-hits)
-        (progn
-          (message "Too many hits! %s" (length entries))
-          nil)
+        (list (lookup-new-entry 'regular dictionary
+                                "��" (format "Error. More than %s hits. (%d)"
+                                             lookup-max-hits (length entries))))
       entries)))
 
 (put 'nducs :content 'nducs-entry-content)
 (defun nducs-entry-content (entry)
   "Return string content of ENTRY."
-  (let* ((char (string-to-char (lookup-entry-code entry))))
-    (with-temp-buffer
-      (insert (format "%c  [U+%04X]\n" char char))
-      (dolist (elt describe-char-unidata-list)
-        (let ((val (get-char-code-property char elt))
-              description)
-          (when val
-            (setq description (char-code-property-description elt val))
-            (insert (if description
-                        (format "  %s: %s (%s)\n" elt val description)
-                      (format "  %s: %s\n" elt val))))))
-      (buffer-substring-no-properties (point-min) (point-max)))))
+  (if (equal (lookup-entry-code entry) "��") "�"
+    (let* ((char (string-to-char (lookup-entry-code entry))))
+      (with-temp-buffer
+        (insert (format "%c  [U+%04X]\n" char char))
+        (dolist (elt describe-char-unidata-list)
+          (let ((val (get-char-code-property char elt))
+                description)
+            (when val
+              (setq description (char-code-property-description elt val))
+              (insert (if description
+                          (format "  %s: %s (%s)\n" elt val description)
+                        (format "  %s: %s\n" elt val))))))
+        (buffer-substring-no-properties (point-min) (point-max))))))
 
 (provide 'nducs)
 
