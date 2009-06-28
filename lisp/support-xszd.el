80e26619c7c929a988e4955a5dc633364c16f560
kawabata <kawabata>
2009-06-28 11:33:52 +0000

    new dictionary support.

---
+++ lisp/support-xszd.el
@@ -0,0 +1,59 @@
+;;; support-xszd.el --- suport file for "學生字典" -*- coding: utf-8 -*-
+;; Copyright (C) 2009 Lookup Development Team
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software Foundation,
+;; Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+;;; Documentation:
+
+;; This file support searching the "學生字典" (商務印書館 / 1915年版)
+;; dictionary file.  The dictionary can be downloaded from the
+;; following URL.
+;;
+;; http://kanji-database.cvs.sourceforge.net/viewvc/kanji-database/kanji-database/dict/xszd.txt
+;;
+;; Execute `mksary' program on "xszd.txt" file to generage
+;; "xszd.txt.ary" suffix array file.
+
+;;; Usage
+;;
+;;  Example:
+;;   (setq lookup-search-agents
+;;         '(
+;;           ....
+;;           (ndsary "~/edicts/xszd/")
+;;           ....))
+
+;;; Code:
+
+(require 'lookup)
+
+(defun support-xszd-arrange-structure (entry)
+  "Attach contents of ENTRY a link and remove tags."
+  (if (looking-at "\\*+")
+      (replace-match ""))
+  (goto-char (point-max))
+  (if (looking-back "\\*+")
+      (replace-match "")))
+
+(setq lookup-support-options
+      (list :title "學生字典"
+            ;; :query-filter 'support-xszd-query-filter
+            :entry-tags   '("**" . "\n")
+            :content-tags '("**" . "\n*")
+            :coding 'utf-8
+            :charsets 'lookup-text-single-cjk-p
+            :arranges '((replace support-xszd-arrange-structure))))
+
+;;; support-xszd.el ends here
