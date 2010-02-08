555e8e04e5357ca3fc5f1845ad86ace1caf7bcff
kawabata <kawabata>
2010-02-08 17:00:12 +0000

    updated to support archived text file.

--- lisp/aozora-view.el
+++ lisp/aozora-view.el
@@ -24,19 +24,27 @@
 
 (require 'lookup)
 (require 'view)
+(require 'ucs-normalize)
 
 ;; Customization
 
-(defvar aozora-view-cache-prefix ".")
+(defvar aozora-view-cache-directory
+  (concat temporary-file-directory "/aozora-view/"))
 (defvar aozora-view-cache-ext    ".cache.gz")
 (defvar aozora-view-save-cache   'prompt) ;; t, nil, 'prompt
 
 ;; Variables
 
-(defvar aozora-view-file nil)
-(make-variable-buffer-local 'aozora-view-file)
+(defvar aozora-view-text-buffer nil
+  "現在の青空ビューバッファに対応するテキストバッファ。")
+(make-variable-buffer-local 'aozora-view-text-buffer)
 
-(defvar aozora-view-buffer nil)
+(defvar aozora-view-text-file nil
+  "現在の青空ビューバッファに対応するテキストファイル。")
+(make-variable-buffer-local 'aozora-view-text-file)
+
+(defvar aozora-view-buffer nil
+  "現在のテキストバッファに対応する青空ビューバッファ。")
 (make-variable-buffer-local 'aozora-view-buffer)
 
 (defvar aozora-view-bookmarks nil)
@@ -49,6 +57,7 @@
     ("白ゴマ傍点" . ?﹆)
     ("丸傍点" .     ?●)
     ("白丸傍点" .   ?○)
+    ("傍点（白丸）" . ?○) ;; 特殊例
     ("黒三角傍点" . ?▲)
     ("白三角傍点" . ?△)
     ("二重丸傍点" . ?◎)
@@ -58,6 +67,22 @@
   (concat "［＃「\\([^」]+?\\)」に"
           (regexp-opt (mapcar 'car aozora-kenten-alist) t)
           "］\n?"))
+
+(define-translation-table 'aozora-accent-table
+  (make-translation-table-from-alist
+   '(([?! ?@] . ?¡) ([?? ?@] . ?¿) 
+     ([?A ?E ?&] . ?Æ) ([?a ?e ?&] . ?æ)
+     ([?O ?E ?&] . ?Œ) ([?o ?e ?&] . ?œ)
+     ([?a ?&] . ?å) ([?A ?&] . ?Å) ([?S ?&] . ?ß)
+     ([?O ?/] . ?Ø) ([?o ?/] . ?ø)
+     (?` . ?\x300)
+     (?' . ?\x301)
+     (?^ . ?\x302)
+     (?~ . ?\x303)
+     (?: . ?\x308)
+     (?& . ?\x30a)
+     (?, . ?\x327)
+     (?_ . ?\x304))))
 
 ;; Keymap
 
@@ -90,7 +115,7 @@ Do not call this directly.  Execute `aozora-view' instead."
   "青空文庫方式に従い、外字を置換し、ルビを設定する。"
   (set-text-properties (point-min) (point-max) nil)
   (goto-char (point-min))
-  (while (re-search-forward "※［[^］]+\\([12]\\)-\\([0-9]+\\)-\\([0-9]+\\)］" nil t)
+  (while (re-search-forward "※［[^］]+\\([12]\\)-\\([0-9]+\\)-\\([0-9]+\\)\\(、.+?\\)?］" nil t)
     (let ((plane  (string-to-number (match-string 1)))
           (row    (+ 32 (string-to-number (match-string 2))))
           (column (+ 32 (string-to-number (match-string 3)))))
@@ -109,6 +134,8 @@ Do not call this directly.  Execute `aozora-view' instead."
   (goto-char (point-min))
   (while (search-forward "／″＼" nil t) (replace-match "〴〵"))
   ;; 圏点（﹅◉﹆﹅◦•）
+  ;; TODO: ルビとの重ね表示への対応
+  ;; TODO: 長文圏点への対応（改行されないのを防ぐ）
   (goto-char (point-min))
   (while (re-search-forward aozora-kenten-regexp nil t)
     (let* ((text (match-string 1))
@@ -131,26 +158,36 @@ Do not call this directly.  Execute `aozora-view' instead."
     (replace-match (propertize (match-string 1)
                                'display '((height 0.5) (raise 1))
                                )))
-  ;; 下付き注記
+  ;; 行右小書き
   (goto-char (point-min))
-  (while (re-search-forward "［＃「\\(.\\)」は下付き小文字］\n?" nil t)
+  (while (re-search-forward "［＃「\\([^」]+\\)」は行右小書き］\n?" nil t)
     (let* ((text (match-string 1)))
       (replace-match "")
       (if (search-backward text nil t)
           (replace-match (propertize text
-                               'display '((height 0.5))))
+                                     'display '((height 0.5))))
+        (error "行右小書き指示の対応テキストが見つかりません！"))))
+  ;; 下付き注記
+  (goto-char (point-min))
+  (while (re-search-forward "［＃「\\(.?\\)」は下付き小文字］\n?" nil t)
+    (let* ((text (match-string 1)))
+      (replace-match "")
+      (if (search-backward text nil t)
+          (replace-match (propertize text
+                                     'display '((height 0.5))))
         (error "下付き指示の対応テキストが見つかりません！"))))
   ;; 上付き注記
   (goto-char (point-min))
-  (while (re-search-forward "［＃「\\(.\\)」は上付き小文字］\n?" nil t)
+  (while (re-search-forward "［＃「\\(.?\\)」は上付き小文字］\n?" nil t)
     (let* ((text (match-string 1)))
       (replace-match "")
       (if (search-backward text nil t)
           (replace-match (propertize text
-                               'display '((height 0.5) (raise 1))))
+                                     'display '((height 0.5) (raise 1))))
         (error "上付き注記指示の対応テキストが見つかりません！"))))
   ;; ママ注記
   ;; e.g. ［＃「喋」に「ママ」の注記］ 
+  ;; TODO: ルビとの重ね表示への対応
   (goto-char (point-min))
   (while (re-search-forward "［＃「\\(.+?\\)」に「ママ」の注記］" nil t)
     (let* ((text (match-string 1)))
@@ -167,21 +204,37 @@ Do not call this directly.  Execute `aozora-view' instead."
       (if (search-backward text nil t)
           (put-text-property (match-beginning 0) (match-end 0)
                              'face 'underline)
-        (error "傍線指示の対応テキストが見つかりません！")
+        nil ;;(error "傍線指示の対応テキストが見つかりません！")
         )))
+  ;; アクセント処理
+  ;; 〔〕：アクセント分解された欧文をかこむ
+  ;; http://www.aozora.gr.jp/accent_separation.html
+  (goto-char (point-min))
+  (when (re-search-forward "〔〕.*アクセント" nil t)
+    ;; アクセント注記がある作品。（なお〔〕の先頭文字はアクセントとみなさない）
+    (while (re-search-forward "〔.\\(.+?\\)〕" nil t)
+      (let ((accented-string (match-string 1)))
+        (replace-match
+         (save-match-data
+           (with-temp-buffer
+             (insert accented-string)
+             (translate-region (point-min) (point-max) 
+                               'aozora-accent-table)
+             (ucs-normalize-NFC-region (point-min) (point-max))
+             (buffer-string)))))))
   ;; 割注処理
   (goto-char (point-min))
   (while (search-forward "［＃ここから割り注］" nil t)
     (let (start end)
       (replace-match "")
       (setq start (point))
-      (if (search-forward "［＃ここで割り注終わり］" nil t)
+      (if (re-search-forward "［＃\\(ここで\\)?割り注終わり］" nil t)
           (progn
             (replace-match "")
             (put-text-property start (point)
                                'display '((height 0.5) (raise 0.5))))
         (error "割注終了指示が見付かりません。"))))
-          
+  
   ;; 字下げ処理
   (goto-char (point-min))
   (while (re-search-forward "［＃ここから\\([0-9０-９]+\\)字下げ］\n?" nil t)
@@ -193,10 +246,15 @@ Do not call this directly.  Execute `aozora-view' instead."
             (put-text-property start (match-beginning 0) 'left-margin (* 2 margin))
             (replace-match ""))
         (error "[字下げ] instruction does not match!"))))
-  ;; その他の指示は削除する。
-  ;; (goto-char (point-min))
-  ;; (while (re-search-forward "［.+?］" nil t) (replace-match ""))
-  ;; 行番号を付与する。
+  ;; その他の指示は別途実装予定：
+  ;; ［＃天からn字下げ］
+  ;; ［＃地からn字上げ］
+  ;; ［＃ここから２字下げ］
+  ;; ［＃ここから改行天付き、折り返してn字下げ］
+  ;; ［＃ここからn字下げ、折り返してn字下げ］
+  ;; ［＃地付き］
+  ;; ［＃改ページ］
+  ;; ［＃ここから横組み］〜［＃ここで横組み終わり］
   (goto-char (point-min))
   (while (re-search-forward "^." nil t)
     (put-text-property (match-beginning 0) (match-end 0) 
@@ -204,7 +262,7 @@ Do not call this directly.  Execute `aozora-view' instead."
   ;; ルビをつけて、改行禁止にする。
   (goto-char (point-min))
   (while (re-search-forward 
-          "\\(\\(?:｜.+?\\)\\|\\(?:[a-z`※㐀-鿿󠄀-󠇿]+[々〻※]?\\)\\)《\\(.+?\\)》" nil t)
+          "\\(\\(?:｜.+?\\)\\|\\(?:[a-z`※㐀-鿿󠄀-󠇿]+々*〻?※?\\)\\)《\\(.+?\\)》" nil t)
     (let ((string (match-string 1))
           (ruby (match-string-no-properties 2)))
       (save-match-data (if (= ?｜ (string-to-char string)) (setq string (substring string 1))))
@@ -229,17 +287,19 @@ Do not call this directly.  Execute `aozora-view' instead."
   "テキストをfillし、ルビを表示する。"
   (goto-char (point-min))
   (let ((adaptive-fill-regexp nil)
-        (view-file aozora-view-file)) ;; save buffer-local-variable
+        (view-file aozora-view-text-file)
+        (view-buffer aozora-view-text-buffer)) ;; save buffer-local-variable
     (while (not (eobp))
-          (lookup-arrange-fill-lines nil))
-    (setq aozora-view-file view-file) ;; restore buffer-local-variable
+      (lookup-arrange-fill-lines nil))
+    (setq aozora-view-text-file view-file
+          aozora-view-text-buffer view-buffer) ;; restore buffer-local-variable
     )
   (goto-char (point-min))
   ;; Rubyを挿入する。
   (let (start end main main-len 
-        ruby-start ruby-end ruby-offset ruby ruby-str
-        ruby-start-width ruby-end-width glue
-        ruby-spc)
+              ruby-start ruby-end ruby-offset ruby ruby-str
+              ruby-start-width ruby-end-width glue
+              ruby-spc)
     (while (not (eobp))
       ;; 各行の処理
       (setq start    (point-at-bol)
@@ -247,12 +307,12 @@ Do not call this directly.  Execute `aozora-view' instead."
             end      (point-at-eol)
             ruby-str "")
       (while (or ;; 現在位置にルビがある場合
-                 (and (get-text-property ruby-end 'ruby)
-                      (setq ruby-start ruby-end))
-                 ;; 現在位置にルビがない場合
-                 (and (setq ruby-start (next-single-char-property-change 
-                                        ruby-end 'ruby nil end))
-                      (not (equal ruby-start end)))) ;; 行末ではない
+              (and (get-text-property ruby-end 'ruby)
+                   (setq ruby-start ruby-end))
+              ;; 現在位置にルビがない場合
+              (and (setq ruby-start (next-single-char-property-change 
+                                     ruby-end 'ruby nil end))
+                   (not (equal ruby-start end)))) ;; 行末ではない
         (message "start=%d, ruby-start=%d" start ruby-start) ;; debug
         (setq ruby       (get-text-property ruby-start 'ruby)
               main-len   (car ruby)
@@ -294,10 +354,8 @@ Do not call this directly.  Execute `aozora-view' instead."
 
 (defun aozora-view-cache-file (file-name)
   "Cache file name for FILE-NAME."
-  (concat (file-name-directory file-name)
-          aozora-view-cache-prefix
-          (file-name-sans-extension
-           (file-name-nondirectory file-name))
+  (concat aozora-view-cache-directory
+          file-name
           aozora-view-cache-ext))
 
 (defun aozora-view-load-cache (file-name)
@@ -311,15 +369,17 @@ Do not call this directly.  Execute `aozora-view' instead."
       t)))
 
 (defun aozora-view-save-cache (file-name)
-  (if (and file-name 
-           (file-exists-p file-name)
-           (if (equal aozora-view-save-cache 'prompt)
+  "現在のバッファ内容を、FILE-NAME のファイルに対応するキャッシュファイルとして保存する。"
+  (let* ((cache-file (aozora-view-cache-file file-name))
+         (cache-dir (file-name-directory cache-file)))
+    (when (if (equal aozora-view-save-cache 'prompt)
                (y-or-n-p "Do you want to save cache file? ")
-             aozora-view-save-cache))
+             aozora-view-save-cache)
       (with-auto-compression-mode
-        (write-region 
-         (prin1-to-string (buffer-string)) nil 
-         (aozora-view-cache-file file-name)))))
+        (make-directory (file-name-directory cache-dir) t)
+        (with-coding-priority '(utf-8-emacs)
+          (write-region 
+           (prin1-to-string (buffer-string)) nil cache-file)) t))))
 
 (defun aozora-view-bookmark (arg)
   (interactive "P")
@@ -329,7 +389,7 @@ Do not call this directly.  Execute `aozora-view' instead."
                  (previous-single-char-property-change 
                   (point) 'line-number)))
             (lax-plist-put 
-             aozora-view-bookmarks (expand-file-name aozora-view-file)
+             aozora-view-bookmarks (expand-file-name aozora-view-text-file)
              (or (get-text-property prev-change-point 'line-number)
                  (get-text-property (1- prev-change-point) 'line-number)))))
     (message "Bookmarked!")))
@@ -339,7 +399,7 @@ Do not call this directly.  Execute `aozora-view' instead."
   (when (eq major-mode 'aozora-view-mode)
     (let* ((line-number
             (lax-plist-get aozora-view-bookmarks
-                           (expand-file-name aozora-view-file)))
+                           (expand-file-name aozora-view-text-file)))
            (pos
             (text-property-any 
              (point-min) (point-max)
@@ -364,52 +424,56 @@ Do not call this directly.  Execute `aozora-view' instead."
 (defun aozora-view-redraw ()
   (interactive)
   (if (eq major-mode 'aozora-view-mode)
-      (let ((file aozora-view-file))
-        (aozora-view-draw)
+      (let ((file aozora-view-text-file)
+            (buffer aozora-view-text-buffer))
+        (aozora-view-draw aozora-view-text-buffer aozora-view-text-file)
         (set-buffer-modified-p nil)
         (aozora-view-mode)
-        (setq aozora-view-file file)
+        (setq aozora-view-text-file file)
         (aozora-view-restore-bookmark))
     (message "Not Aozora-View mode!")))
 
-(defun aozora-view-draw ()
-  "Draw Aozora text of `aozora-view-file'."
+(defun aozora-view-draw (text-buffer text-file-name)
+  "現バッファに、TEXT-BUFFER または TEXT-FILE-NAME にあるテキスト
+ファイルを描画する。"
   (let ((inhibit-read-only t))
-    (if (or (null aozora-view-file)
-            (not (file-exists-p aozora-view-file)))
-        (error "Aozora Text File does not exist!"))
     (delete-region (point-min) (point-max))
-    (insert-file-contents aozora-view-file)
+    (if (buffer-live-p text-buffer)
+        (insert (with-current-buffer text-buffer 
+                  (buffer-string)))
+      (if (and text-file-name
+               (file-exists-p text-file-name))
+          (with-coding-priority '(cp932 utf-8)
+            (insert-file-contents text-file-name))
+        (error "Aozora Text File does not exist!")))
     (goto-char (point-min))
     (while (search-forward "" nil t) (replace-match ""))
     (aozora-view-arrange-replace)
     (aozora-view-arrange-fill-lines)
     (set-buffer-modified-p nil)
-    (aozora-view-save-cache aozora-view-file)))
+    (aozora-view-save-cache text-file-name)))
 
 (defun aozora-view ()
   (interactive)
-  "青空文庫ファイルを整形して表示する。
-.txt 拡張子のテキストファイルのみ対象。"
+  "現バッファの内容を、青空文庫フォーマットとみなし、別バッファに整形して表示する。"
   (if (and (equal major-mode 'text-mode)
-           (buffer-file-name)
-           (string-match "\\.txt$" (buffer-file-name)))
-      (let ((text-buffer        (current-buffer))
+           (string-match "\\.txt" (buffer-file-name)))
+      (let ((text-buffer    (current-buffer))
             (text-file-name (buffer-file-name)))
         (when (or (null aozora-view-buffer)
                   (not (buffer-live-p aozora-view-buffer)))
+          ;; 青空ビューバッファを作成する。
           (with-current-buffer
               (setq aozora-view-buffer
                     (generate-new-buffer
                      (file-name-sans-extension
                       (file-name-nondirectory text-file-name))))
             (or (aozora-view-load-cache text-file-name)
-                (progn
-                  (setq aozora-view-file text-file-name)
-                  (aozora-view-draw)))))
+                (aozora-view-draw text-buffer text-file-name))))
         (switch-to-buffer aozora-view-buffer)
         (aozora-view-mode)
-        (setq aozora-view-file text-file-name)
+        (setq aozora-view-text-buffer text-buffer
+              aozora-view-text-file text-file-name)
         (aozora-view-restore-bookmark))
     (message "Buffer is not `*.txt' text-mode.")))
 
@@ -3762,11 +3826,14 @@ Do not call this directly.  Execute `aozora-view' instead."
   ("丸E大文字" . "Ⓔ")
   ("丸Z大文字" . "Ⓩ")
   ("「オンス」の単位記号" . "℥")
-  ("「卍」を左右反転したもの" . "卐"))))
+  ("「卍」を左右反転したもの" . "卐")
+  ;; 疑似置換
+  ("毋の真ん中の縦棒が下につきぬけたもの" . "毋")
+  ("「褒」の「保」に代えて「丑」" . "衰")
+  ("感嘆符三つ" . "!!!")
+  )))
   (dolist (pair gaiji-data)
     (puthash (car pair) (cdr pair) aozora-view-gaiji-table)))
-  
-
 
 (provide 'aozora-view)
               
