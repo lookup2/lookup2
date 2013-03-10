3ded76d5a743994c6a4d630d7ccbba3d78c6323f
Taichi KAWABATA <kawabata.taichi@gmail.com>
2013-03-10 12:10:16 +0900

    new support files.

---
+++ lisp/support-files/support-pdic-thai.el
@@ -0,0 +1,25 @@
+;;; support-pdic-thai.el --- Support for PDIC Thai -*- lexical-binding: t -*-
+
+;; Copyright (c) KAWABATA, Taichi
+
+;;; Commentary:
+
+;; support for PDIC Thai
+;; http://www.pdicthai.com/~pdicthai/
+
+;;; Code:
+
+(require 'lookup)
+(eval-when-compile (require 'cl-lib))
+
+(defvar support-pdic-thai-title-alist
+  '(("Uni-PdicThai-IPA" :title "タイ日辞書")
+    ("Uni-PdicThai-JP-IPA" :title "タイ日辞書（かな）" 
+     :query-filter lookup-query-filter-kanji-to-kana)
+    ("Uni-PdicThai-RS-IPA" :title "タイ日辞書（ローマ字）")))
+
+(setq lookup-support-options
+      (cdr
+       (cl-assoc-if 
+        (lambda (x) (string-match x lookup-support-dictionary-id))
+        support-pdic-thai-title-alist)))
