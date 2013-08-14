749153c7b0d5f9d18c25c7c6151c81ea8c0e7257
Kazuhiro Ito <kzhr@d1.dion.ne.jp>
2013-08-14 20:53:40 +0900

    Fix the byte-compiling problem when Emacs's .el files are not installed.

--- lisp/hanzi-py-table.el
+++ lisp/hanzi-py-table.el
@@ -13,23 +13,66 @@
 
 ;;; Code:
 
+(eval-when-compile
+  (defun hanzi-py-table-build-internal (elt key)
+    (let ((target (nth 1 elt)))
+      (setq key (concat key (char-to-string (car elt))))
+      (apply
+       'nconc
+       (cond 
+	((integerp target)
+	 (list (cons key (list target))))
+	(target
+	 (list
+	  (cons key
+		(delq nil
+		      (mapcar
+		       (lambda (elt) (when (stringp elt)
+				       (string-to-char elt)))
+		       (if (consp target) (cdr target) target)))))))
+       (mapcar (lambda (elt)
+		 (hanzi-py-table-build-internal elt key))
+	       (cddr elt))))))
+
 (defvar hanzi-py-table
-  (eval-when-compile
-    (let ((table (make-char-table 'char-code-property-table)))
-      (dolist (file (list (locate-library "quail/TONEPY.el")
-                          (locate-library "quail/PY-b5.el")))
-        (with-temp-buffer 
-          (insert-file-contents file)
-          (re-search-forward "(quail-define-rules")
-          (goto-char (match-beginning 0))
-          (dolist (item (cdar (read-from-string (buffer-substring (point) (point-max)))))
-            (let ((py (car item)) (str (cadr item)))
-              (dolist (char (string-to-list str))
-                (aset table char (push py (aref table char))))))))
-      (map-char-table (lambda (k v) (setq v (delete-dups v))
-                        (when (= 1 (length v)) (aset table k (car v))))
-                      table)
-      table)))
+  (let ((table (make-char-table 'char-code-property-table))
+	(alist
+	 (eval-when-compile
+	   (apply
+	    'nconc
+	    (mapcar
+	     (lambda (method)
+	       (let ((src (locate-library (concat (cadr method) ".el"))))
+		 (if src
+		     (with-temp-buffer 
+		       (message "Reading file %s" src)
+		       (insert-file-contents src)
+		       (re-search-forward "(quail-define-rules")
+		       (mapcar (lambda (elt)
+				 (cons (car elt) (string-to-list (cadr elt))))
+			       (cdar (read-from-string
+				      (buffer-substring (match-beginning 0)
+							(point-max))))))
+		   (unwind-protect
+		       (progn
+			 (apply 'quail-use-package method)
+			 (apply 'nconc
+				(mapcar
+				 (lambda (elt)
+				   (when elt
+				     (hanzi-py-table-build-internal elt nil)))
+				 (nth 2 (quail-package (car method))))))
+		     (quail-deactivate)))))
+	     '(("chinese-tonepy" "quail/TONEPY")
+	       ("chinese-py-b5" "quail/PY-b5")))))))
+    (dolist (item alist)
+      (let ((py (car item)) (charlist (cdr item)))
+	(dolist (char charlist)
+	  (aset table char (push py (aref table char))))))
+    (map-char-table (lambda (k v) (setq v (delete-dups v))
+		      (when (= 1 (length v)) (aset table k (car v))))
+		    table)
+    table))
 
 (set-char-table-extra-slot hanzi-py-table 0 'hanzi-pinyin)
 (define-char-code-property 'hanzi-pinyin hanzi-py-table)
